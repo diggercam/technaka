0344506510c5de41b015c1f825b1c0b9e79d9ba7
Camille Kerbaul <camille.kerbaul@gadz.org>
2020-01-07 23:08:59 +0100

    commit

--- camera.py
+++ camera.py
@@ -7,8 +7,8 @@ camera = PiCamera()
 camera.start_preview()
 sleep(1)
 for filename in camera.capture_continuous('./img/img{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
-print('Captured %s' % filename)
-sleep(1)
+	print('Captured %s' % filename)
+	sleep(1)
 
 
 camera.stop_preview()
